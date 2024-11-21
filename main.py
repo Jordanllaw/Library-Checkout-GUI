6067140cbd1af50cef92eeace0b37db47672d4ff
Jordannll <jorlaw4753@gmail.com>
2024-11-20 23:13:37 -0500

    Sign in window completed
    The sign in window now opens when the program is run, you can then input anything and it is verified whether it is a valid ID, password or user status. This then logs the user into the appropriate screen

--- main.py
+++ main.py
@@ -1,48 +1,6 @@
 
 from tkinter import *
-import tkinter as tk
 import csv
-
-# helper functions
-def check_empty(s: StringVar):
-    if s.get() == "":
-        return True
-    return False
-
-# def read_data():
-#     table = []
-#     with open('Library Inventory.csv', mode ='r')as file:
-#         csvFile = csv.reader(file)
-#         for line in csvFile:
-#             table.append(line)
-#     file.close()
-#     # Initialize the data frame
-#     for widget in data_frame.winfo_children():
-#         widget.destroy()
-#     # Now table is the csv file in nested-list form
-#     for row_num in range(len(table)):
-#         for column_num in range(len(table[row_num])):
-#             cell_value = table[row_num][column_num]
-#             cell_label = Label(data_frame, text=cell_value)
-#             cell_label.grid(row=row_num, column=column_num)
-
-# def write_data():
-#     table = []
-#     with open('Library Inventory.csv', mode ='r', newline='') as file:
-#         csvFile = csv.reader(file)
-#         for line in csvFile:
-#             table.append(line)
-#     file.close()
-#     # Now table is the csv file in nested-list form
-#     # Update the table and do the write
-#     table.append([first_name_value.get(), last_name_value.get(),
-#     student_number_value.get()])
-#     with open('Library Inventory.csv', mode ='w', newline='') as file:
-#         writer = csv.writer(file)
-#         writer.writerows(table)
-#     file.close()
-
-########## SCREEN ##########
 
 # base window
 window = Tk()
@@ -51,83 +9,81 @@ window.geometry('200x200')
 password = StringVar(window)
 id = StringVar(window)
 
-# fonts
-# style1 = font.Font(size=25)
-# style2 = font.Font(size=12)
-
 # modes
-def mode_chooser():
-    with open("Users.csv", "r") as users:
-        curr_user_id = None
-        curr_user_pass = None
-        curr_user_status = None
-        users.readline()
-        accounts = users.readlines()
-        print(accounts)
-        for a in accounts:
-            a = a.split(',')
-            if a[0] == id:
-                curr_user_id = a[0]
-                if a[1] == password:
-                    curr_user_pass = a[1]
-                    curr_user_status = a[2]
-                    break
-                else:
-                    print("incorrect password")
-                    break
-    if curr_user_id is None:
-        print("No user found, ask an employee to create one")
+def customer_screen():
+    customer_frame = Frame(window, width=500, height=700, bg="yellow")
+    customer_frame.grid(row=0, column=0)
 
+def employee_screen():
+    employee_frame = Frame(window, width=500, height=700, bg="red")
+    employee_frame.grid(row=0, column=0)
 
 def sign_in_menu():
-    window.title("Sign In")
-
     # initial frame
-    input_frame = LabelFrame(window, text="Sign in")
-    input_frame.grid(row=0, column=0)
+    sign_in_frame = Frame(window, width=200, height=200, name="sign in")
+    sign_in_frame.grid(row=0, column=0)
 
     # id label and entry
-    id_label = Label(input_frame, text="ID")
+    id_label = Label(sign_in_frame, text="ID")
     id_label.grid(row=0, column=0)
-    id_entry = Entry(input_frame, textvariable=id)
+    id_entry = Entry(sign_in_frame, textvariable=id)
     id_entry.grid(row=0, column=1)
 
     # password label and entry
-    password_label = Label(input_frame, text="Password")
+    password_label = Label(sign_in_frame, text="Password")
     password_label.grid(row=1, column=0)
-    password_entry = Entry(input_frame, textvariable=password)
+    password_entry = Entry(sign_in_frame, textvariable=password)
     password_entry.grid(row=1, column=1)
 
     # valid entry label
-    valid_label = Label(input_frame)
+    global invalid_label
+    invalid_label = Label(sign_in_frame)
+    invalid_label.grid(row=2, column=1)
 
     # sign in button
-    sign_in_button = Button(input_frame, text="Sign in", command=mode_chooser)
-    sign_in_button["state"] = "disabled"
-    sign_in_button.grid(row=2, column=1)
-    tk.update_idletasks()
-    tk.update()
-    if not(check_empty(password)) and not(check_empty(id)):
-        sign_in_button["state"] = "enabled"
+    sign_in_button = Button(sign_in_frame, text="Sign in", command=validate_entry)
+    sign_in_button.grid(row=3, column=1)
 
-# first_name_value = StringVar(window)
-# last_name_value = StringVar(window)
-# student_number_value = StringVar(window)
-# data_frame = LabelFrame(window, text="Books in our Library")
-# data_frame.grid(row=6, column=0)
-# input_frame = LabelFrame(window, text="Book Search")
-# input_frame.grid(row=0, column=0)
-# first_name_label = Label(input_frame, text="Book Title")
-# first_name_label.grid(row=0, column=0)
-# first_name_entry = Entry(input_frame, textvariable=first_name_value)
-# first_name_entry.grid(row=0, column=1)
-# last_name_label = Label(input_frame, text="Author")
-# last_name_label.grid(row=1, column=0)
-# last_name_entry = Entry(input_frame, textvariable=last_name_value)
-# last_name_entry.grid(row=1, column=1)
-# submit_button = Button(input_frame, text='Checkout', command=write_data)
-# submit_button.grid(row=3, column=1)
-# show_button = Button(input_frame, text='Show Inventory', command=read_data)
-# show_button.grid(row=3, column=0) 
+# helper functions
+def validate_entry():
+    curr_user_id = None
+    curr_user_pass = None
+    curr_user_status = None
+    if not id.get().isnumeric():
+        print("Invalid ID")
+        invalid_label.config(text="Invalid ID")
+        return
+    with open("Users.csv", "r") as users:
+        users.readline()
+        accounts = users.readlines()
+        for a in accounts:
+            a = a.split(',')
+            if a[0] == id.get():
+                curr_user_id = a[0].strip()
+                if a[1] == password.get():
+                    curr_user_pass = a[1].strip()
+                    curr_user_status = a[2].strip()
+                    if curr_user_status == "employee":
+                        employee_screen()
+                        return
+                    elif curr_user_status == "customer":
+                        customer_screen()
+                        return
+                    else:
+                        invalid_label.config(text="Invalid user status,\nupdate users file")
+                        print("Invalid user status, update users file")
+                    return
+                else:
+                    if password.get() == "":
+                        print("Invalid password")
+                        invalid_label.config(text="Invalid password")
+                    else:
+                        print("Incorrect password")
+                        invalid_label.config(text="Incorrect password")
+                    break
+    if curr_user_id is None:
+        print("No user found, ask an employee to create one")
+        invalid_label.config(text="No user found,\nask an employee to\ncreate one")
+
 sign_in_menu()
-mainloop()
+window.mainloop()
